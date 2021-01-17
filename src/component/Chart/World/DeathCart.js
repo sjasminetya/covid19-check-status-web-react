213ccb9d8cee08413242b973c1a8cf2e1b5e4051
sjasminetya <shaulajasminetya22@gmail.com>
2021-01-17 17:51:40 +0700

    fix footer

---
+++ src/component/Chart/World/DeathCart.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react'
+import Chart from 'chart.js'
+import axios from 'axios'
+import moment from 'moment'
+
+export default class DeathCart extends Component {
+    chartRef = React.createRef()
+
+    componentDidMount() {
+        const myChartRef = this.chartRef.current.getContext('2d')
+        let date = []                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                             
+        let death = []
+
+        axios.get(`${process.env.REACT_APP_API_COVID19}world?from=2020-03-01&to=${new Date()}`)
+        .then(res => {
+            res.data.map((item) =>
+                death.push(item.TotalDeaths) 
+            )
+            res.data.map((item) =>
+                date.push(moment(item.Date).format("MMM Do YY")) 
+            )
+            new Chart(myChartRef, {
+                type: 'line',
+                data: {
+                    labels: date,
+                    datasets: [
+                        {
+                            label: 'Death',
+                            data: death,
+                            fill: false,
+                            borderColor: "#F01159"
+                        }
+                    ]
+                }
+            })
+        })
+        .catch(err => {
+            console.log(err)
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <canvas id='myChart' ref={this.chartRef}></canvas>
+            </div>
+        )
+    }
+}
--- src/component/Footer/Footer.js
+++ src/component/Footer/Footer.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
+import styles from './Footer.module.css'
 
 export default class Footer extends Component {
     render() {
         return (
             <div>
                 <footer className="py-2 bg-dark text-white">
-                    <h3 className="text-center">Stay safe, stay healty</h3>
+                    <h3 className={styles['text-footer']}>Stay safe, stay healty &hearts;</h3>
                 </footer>
             </div>
         )
